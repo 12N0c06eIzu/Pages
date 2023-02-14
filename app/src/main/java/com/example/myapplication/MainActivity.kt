20a1d38ffa8dd3f328904dc629a2eeb92c5d02b8
12N0c06Izu <ritu6811@gmail.com>
2023-02-14 21:07:59 +0900

    Navigationコンポーネント配置

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +22,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                ) {
                     var human: String = "Player"
                     Greeting(human)
                 }
@@ -29,13 +35,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    Column() {
+
+
     Text(text = "Hello $name!")
+    Text("kasanaru")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
+        Column() {
+
         Greeting("Android")
+        BottomNavigation()
+        }
+    }
+}
+
+@Composable
+fun BottomNavigation() {
+
+    var selectedItem = remember { mutableStateOf(0) }
+    // Navigationの表示項目
+    val items = listOf("Home", "Search", "Notification", "Settings")
+
+    // BottomNavigation横幅
+    BottomNavigation {
+        // Sepalater　itemsの数、右に並ぶ
+        items.forEachIndexed { index, item ->
+            // Iconの設定
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                label = { Text(item) },
+                selected = selectedItem.value == index,
+                onClick = { selectedItem.value = index }
+            )
+        }
     }
 }
